0243ddb614834c52821c62b3403769948b253c0e
Walter Castro <walterkstro@gmail.com>
2018-12-27 19:18:30 -0600

    Soporte para json default V4 Webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import css from './main.css'
 import scss from './scss/main.scss'
 
+
 import {
    operar
 } from './operacion'
+import data from './data.json'
 
 
 let saludo = "Hello"
@@ -12,3 +14,4 @@ let saludo = "Hello"
 console.log('Hola Webpack')
 console.log(`${saludo} mundo`)
 console.log(`La suma es: ${operar(10, 10)}`)
+console.log(data)
